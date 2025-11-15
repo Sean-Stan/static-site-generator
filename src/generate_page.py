2ba5be30740ea8a709dfbf4da99c9cc02b5f857a
Sean-Stan <mav1120@hotmail.com>
2025-11-15 14:23:40 -0600

    fixed hardcoded paths and added basepath

--- src/generate_page.py
+++ src/generate_page.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
         
     raise Exception("No h1 header found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     file = open(from_path)
     markdown = file.read()
@@ -21,16 +21,17 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     html = markdown_to_html_node(markdown).to_html()
     full_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    full_html = full_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(full_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     content_files = os.listdir(dir_path_content)
     
     for file in content_files:
         file_or_folder = os.path.join(dir_path_content, file)
         if os.path.isfile(file_or_folder):
-            generate_page(file_or_folder, template_path, os.path.join(dest_dir_path, os.path.splitext(file)[0] + ".html"))
+            generate_page(file_or_folder, template_path, os.path.join(dest_dir_path, os.path.splitext(file)[0] + ".html"), basepath)
         else:
-            generate_pages_recursive(file_or_folder, template_path, os.path.join(dest_dir_path, file))
+            generate_pages_recursive(file_or_folder, template_path, os.path.join(dest_dir_path, file), basepath)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from copy import copy_source_to_destination
+import sys
 from generate_page import generate_pages_recursive
 
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 def main():
-    copy_source_to_destination('/home/sean/static-site-generator/static', '/home/sean/static-site-generator/public')
-    generate_pages_recursive("/home/sean/static-site-generator/content/", "/home/sean/static-site-generator/template.html", "/home/sean/static-site-generator/public/")
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    copy_source_to_destination(dir_path_static, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
