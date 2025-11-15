0ff842692f38ab51dad3b77cae29001a15cf2dee
Sean-Stan <mav1120@hotmail.com>
2025-11-15 14:32:43 -0600

    added build.sh and docs/, updated to build to docs/

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sys
 from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
